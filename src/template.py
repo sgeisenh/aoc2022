c8dcd9a109004a253431e4ca5cab55cebf5b4ef9
Samuel Eisenhandler <sgeisenhandler@gmail.com>
2022-11-30 10:30:44 -0500

    Template and .gitignore

---
+++ src/template.py
@@ -0,0 +1,12 @@
+import fileinput
+
+lines = [line.strip() for line in fileinput.input()]
+nums = [int(line) for line in lines]
+
+p1 = 0
+
+print("Part one:", p1)
+
+p2 = 0
+
+print("Part two:", p2)
